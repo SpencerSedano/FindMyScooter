5396d3f4105fcc9c992259489302f37388a1f929
SpencerSedano <spencer.sv20@gmail.com>
2026-06-13 18:30:44 +0800

    update volume buzzer

--- src/main.c
+++ src/main.c
@@ -41,6 +41,92 @@ static struct gpio_callback button1_cb_data;
 
 static struct k_work play_song_work;
 
+// --- Melody ---
+#define NOTE_GS4  415   // G#4
+#define NOTE_A4   440   // A4
+#define NOTE_B4   494   // B4
+#define NOTE_CS5  554   // C#5
+#define NOTE_DS5  622   // D#5
+#define NOTE_E5   659   // E5
+#define NOTE_FS5  740   // F#5
+#define NOTE_GS5  830   // G#5
+#define NOTE_E4   330   // E4
+#define NOTE_FS4  370   // F#4
+#define NOTE_DS4  311   // D#4
+
+// Define a structure to hold each note and its duration
+struct Note {
+  uint32_t frequency_hz;
+  uint32_t duration_ms;
+};
+
+// Jingle Bells melody array
+struct Note christmas_melody[] = {
+  // --- Part 1 ---
+  {NOTE_GS4, 300},   // G#4
+  {NOTE_A4,  300},   // A4
+  {NOTE_B4,  500},   // B4
+  {NOTE_B4,  500},   // B4
+  {NOTE_CS5, 300},   // C#5
+  {NOTE_DS5, 300},   // D#5
+  {NOTE_E5,  500},   // E5
+  {NOTE_E5,  500},   // E5
+  {NOTE_GS4, 300},   // G#4
+  {NOTE_A4,  300},   // A4
+  {NOTE_B4,  500},   // B4
+  {NOTE_B4,  500},   // B4
+  {NOTE_CS5, 300},   // C#5
+  {NOTE_B4,  300},   // B4
+  {NOTE_A4,  500},   // A4
+  {NOTE_A4,  500},   // A4
+  {NOTE_GS4, 300},   // G#4
+  {NOTE_B4,  300},   // B4
+  {NOTE_E4,  300},   // E4
+  {NOTE_GS4, 300},   // G#4
+  {NOTE_FS4, 500},   // F#4
+  {NOTE_A4,  500},   // A4
+  {NOTE_DS4, 600},   // D#4
+  {NOTE_E4,  600},   // E4
+
+  // --- Part 2 (all +, octave 5) ---
+  {NOTE_E5,  300},   // E5
+  {NOTE_FS5, 300},   // F#5
+  {NOTE_E5,  300},   // E5
+  {NOTE_DS5, 300},   // D#5
+  {NOTE_E5,  300},   // E5
+  {NOTE_CS5, 300},   // C#5
+  {NOTE_CS5, 300},   // C#5
+
+  {NOTE_E5,  300},   // E5
+  {NOTE_FS5, 300},   // F#5
+  {NOTE_E5,  300},   // E5
+  {NOTE_DS5, 300},   // D#5
+  {NOTE_E5,  300},   // E5
+  {NOTE_CS5, 300},   // C#5
+
+  {NOTE_FS5, 300},   // F#5
+  {NOTE_GS5, 300},   // G#5
+  {NOTE_FS5, 300},   // F#5
+  {NOTE_E5,  300},   // E5
+  {NOTE_FS5, 300},   // F#5
+  {NOTE_DS5, 300},   // D#5
+  {NOTE_DS5, 300},   // D#5
+
+  {NOTE_DS5, 300},   // D#5
+  {NOTE_E5,  300},   // E5
+  {NOTE_FS5, 300},   // F#5
+  {NOTE_E5,  300},   // E5
+  {NOTE_DS5, 300},   // D#5
+  {NOTE_CS5, 300},   // C#5
+
+  // --- Part 3 (no +, octave 4) ---
+  {NOTE_B4,  500},   // B4
+  {NOTE_A4,  500},   // A4
+  {NOTE_GS4, 500},   // G#4
+  {NOTE_FS4, 600},   // F#4
+};
+
+
 /* --- BLE CONFIGURATION --- */
 static const struct bt_data ad[] = {
     BT_DATA_BYTES(BT_DATA_FLAGS, (BT_LE_AD_GENERAL | BT_LE_AD_NO_BREDR)),
@@ -56,19 +142,51 @@ static const struct bt_le_adv_param* adv_params = BT_LE_ADV_CONN_FAST_1;
 
 // Passive Buzzer
 static const struct pwm_dt_spec buzzer = PWM_DT_SPEC_GET(DT_NODELABEL(buzzer_pwm));
+static const struct pwm_dt_spec buzzer_inv = PWM_DT_SPEC_GET(DT_NODELABEL(buzzer_pwm_inv));
 
+
+// void play_beep(uint32_t frequency_hz, uint32_t duration_ms) {
+
+//   if (frequency_hz == 0) {
+//     pwm_set_dt(&buzzer, 0, 0);
+//   }
+//   else {
+//     uint32_t period_ns = 1000000000 / frequency_hz;
+//     uint32_t pulse_ns = period_ns / 2;
+//     pwm_set_dt(&buzzer, period_ns, pulse_ns);
+//   }
+
+//   k_msleep(duration_ms);
+//   pwm_set_dt(&buzzer, 0, 0);
+// }
 void play_beep(uint32_t frequency_hz, uint32_t duration_ms) {
   if (frequency_hz == 0) {
     pwm_set_dt(&buzzer, 0, 0);
+    pwm_set_dt(&buzzer_inv, 0, 0);
   }
   else {
     uint32_t period_ns = 1000000000 / frequency_hz;
     uint32_t pulse_ns = period_ns / 2;
     pwm_set_dt(&buzzer, period_ns, pulse_ns);
+    pwm_set_dt(&buzzer_inv, period_ns, pulse_ns);
   }
 
   k_msleep(duration_ms);
+
   pwm_set_dt(&buzzer, 0, 0);
+  pwm_set_dt(&buzzer_inv, 0, 0);
+}
+
+void play_christmas_lights() {
+  int num_notes = sizeof(christmas_melody) / sizeof(christmas_melody[0]);
+
+  for (int i = 0; i < num_notes; i++) {
+    // Play the current note
+    play_beep(christmas_melody[i].frequency_hz, christmas_melody[i].duration_ms);
+
+    // Add a tiny 50ms silence between notes so they do not blend together into one long beep
+    play_beep(0, 50);
+  }
 }
 
 /* --- CALLBACKS --- */
@@ -106,33 +224,35 @@ void button_pressed1(const struct device* dev, struct gpio_callback* cb, uint32_
 void play_song_handler(struct k_work* work) {
   printk("Playing Christmas Lights Song...\n");
 
-  /* Jin-gle bells */
-  play_beep(1319, 80);
-  k_msleep(90);
-  play_beep(1319, 80);
-  k_msleep(90);
-  play_beep(1319, 160);
-  k_msleep(170);
+  // /* Jin-gle bells */
+  // play_beep(1319, 80);
+  // k_msleep(90);
+  // play_beep(1319, 80);
+  // k_msleep(90);
+  // play_beep(1319, 160);
+  // k_msleep(170);
 
-  /* Jin-gle bells */
-  play_beep(1319, 80);
-  k_msleep(90);
-  play_beep(1319, 80);
-  k_msleep(90);
-  play_beep(1319, 160);
-  k_msleep(170);
+  // /* Jin-gle bells */
+  // play_beep(1319, 80);
+  // k_msleep(90);
+  // play_beep(1319, 80);
+  // k_msleep(90);
+  // play_beep(1319, 160);
+  // k_msleep(170);
 
-  /* Jin-gle all the way */
-  play_beep(1319, 80);
-  k_msleep(90);
-  play_beep(1568, 80);
-  k_msleep(90);
-  play_beep(1047, 80);
-  k_msleep(90);
-  play_beep(1175, 80);
-  k_msleep(90);
-  play_beep(1319, 240);
-  k_msleep(250);
+  // /* Jin-gle all the way */
+  // play_beep(1319, 80);
+  // k_msleep(90);
+  // play_beep(1568, 80);
+  // k_msleep(90);
+  // play_beep(1047, 80);
+  // k_msleep(90);
+  // play_beep(1175, 80);
+  // k_msleep(90);
+  // play_beep(1319, 240);
+  // k_msleep(250);
+
+  play_christmas_lights();
 }
 
 /* --- MAIN --- */
